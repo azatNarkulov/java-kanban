a7b70d9ee9b456b1294997566be9bd6e5a7bd82b
Naritab_ <rzekicc@gmail.com>
2025-04-13 19:24:45 +0600

    Refactor TaskManager, Task, Epic etc.

--- src/ru/yandex/practicum/Main.java
+++ src/ru/yandex/practicum/Main.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.manager.TaskManager;
+import ru.yandex.practicum.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,46 +10,91 @@ public class Main {
         TaskManager taskManager = new TaskManager();
 
         Task task1 = new Task("Тестовая задача", "Описание тестовой задачи", Status.NEW);
-        taskManager.addTask(task1);
+        Integer result = taskManager.addTask(task1);
+        if (result != null) {
+            System.out.println("Задача с id-номером " + result + " успешно добавлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
         Task task2 = new Task("Тестовая задача #2", "Описание тестовой задачи #2", Status.NEW);
-        taskManager.addTask(task2);
+        result = taskManager.addTask(task2);
+        if (result != null) {
+            System.out.println("Задача с id-номером " + result + " успешно добавлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
 
         Epic epic1 = new Epic("Эпик с двумя подзадачами", "Описание эпика с двумя подзадачами");
-        taskManager.addEpic(epic1);
+        result = taskManager.addEpic(epic1);
+        if (result != null) {
+            System.out.println("Эпик с id-номером " + result + " успешно добавлен");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
         Subtask subtask1 = new Subtask("Подзадача №1", "Описание подзадачи №1", epic1.getId());
-        taskManager.addSubtask(subtask1);
+        result = taskManager.addSubtask(subtask1);
+        if (result != null) {
+            System.out.println("Подзадача с id-номером " + result + " успешно добавлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
         Subtask subtask2 = new Subtask("Подзадача №2", "Описание подзадачи №2", epic1.getId());
-        taskManager.addSubtask(subtask2);
+        result = taskManager.addSubtask(subtask2);
+        if (result != null) {
+            System.out.println("Подзадача с id-номером " + result + " успешно добавлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
 
         Epic epic2 = new Epic("Эпик с одной подзадачей", "Описание эпика с одной подзадачей");
-        taskManager.addEpic(epic2);
+        result = taskManager.addEpic(epic2);
+        if (result != null) {
+            System.out.println("Эпик с id-номером " + result + " успешно добавлен");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
         Subtask subtask3 = new Subtask("Подзадача №3", "Описание подзадачи №1", epic2.getId());
-        taskManager.addSubtask(subtask3);
+        result = taskManager.addSubtask(subtask3);
+        if (result != null) {
+            System.out.println("Подзадача с id-номером " + result + " успешно добавлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
 
-        System.out.println(taskManager.printAllTasks());
-        System.out.println(taskManager.printAllEpiks());
-        System.out.println(taskManager.printAllSubtasks());
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubtasks());
 
         task1.setTitle("Тестовая задача с др стат-м");
         task1.setStatus(Status.IN_PROGRESS);
-        taskManager.updateTask(task1);
+        result = taskManager.updateTask(task1);
+        if (result != null) {
+            System.out.println("Задача с id-номером " + result + " успешно обновлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
 
         subtask1.setTitle("Подзадача №1 c др стат-м");
         subtask1.setStatus(Status.IN_PROGRESS);
-        taskManager.updateSubtask(subtask1);
+        result = taskManager.updateSubtask(subtask1);
+        if (result != null) {
+            System.out.println("Подзадача с id-номером " + result + " успешно обновлена");
+        } else {
+            System.out.println("Произошла ошибка");
+        }
 
         System.out.println();
-        System.out.println(taskManager.printAllTasks());
-        System.out.println(taskManager.printAllEpiks());
-        System.out.println(taskManager.printAllSubtasks());
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubtasks());
 
         taskManager.deleteTask(task1.getId());
         taskManager.deleteEpic(epic1.getId());
         taskManager.deleteSubtask(subtask3.getId());
 
         System.out.println();
-        System.out.println(taskManager.printAllTasks());
-        System.out.println(taskManager.printAllEpiks());
-        System.out.println(taskManager.printAllSubtasks());
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubtasks());
     }
 }
